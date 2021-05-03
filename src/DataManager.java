045bbea845da3c95f301ac02cabc29059a9825e3
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-05-03 10:52:22 -0400

    Fixed HashMap?

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,15 +1,19 @@
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class DataManager {
-    private HashMap<Item,LinkedList> library;
+    private HashMap<Integer, Item> library;
     private LinkedList<Book> books;
     private LinkedList<Music> music;
     private LinkedList<Movie> movies;
-    public DataManager(){
+    private Scanner scan;
+
+    public DataManager() {
         this.books = new LinkedList<>();
         this.music = new LinkedList<>();
         this.movies = new LinkedList<>();
         this.library = new HashMap<>();
+        this.scan = new Scanner(System.in);
     }
 }
