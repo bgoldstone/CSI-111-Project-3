dd1d7f7ec3b6882d1a764e038d67f6f1f6848700
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-05-05 11:00:01 -0400

    Refactor Renamed

--- src/GUI.java
+++ src/GUI.java
@@ -67,4 +67,70 @@ public class GUI extends JFrame {
         JFrame frame = new GUI("Library");
         frame.setVisible(true);
     }
+
+    public static class TabbedGUI extends JFrame {
+        private JPanel panel;
+        private JButton checkOutButton;
+        private JButton checkInButton;
+        private JButton loadFileButton;
+        private JButton itemListingsButton;
+        private JButton availableCopiesButton;
+        private JButton saveFileButton;
+        private DataManagerGUI dm;
+
+        public TabbedGUI(String title) {
+            super(title);
+            this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+            this.setContentPane(panel);
+            this.pack();
+            dm = new DataManagerGUI();
+
+            //Sets size
+            panel.setPreferredSize(new Dimension(500, 500));
+
+            //Basically centers on screen
+            this.setLocationRelativeTo(null);
+            loadFileButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.loadFile();
+                }
+            });
+            saveFileButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.saveFile();
+                }
+            });
+            checkOutButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.checkOut();
+                }
+            });
+            checkInButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.checkIn();
+                }
+            });
+            availableCopiesButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.getNumberOfCopies();
+                }
+            });
+            itemListingsButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dm.getItemType();
+                }
+            });
+        }
+
+        public static void main(String[] args) {
+            JFrame frame = new TabbedGUI("Library");
+            frame.setVisible(true);
+        }
+    }
 }
